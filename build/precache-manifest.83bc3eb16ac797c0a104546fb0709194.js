c4246b86a31f41bc5dfe8424b13f8e9a316ffdbb
ZukaGap <zurabigaprindashvili@gmail.com>
2020-06-22 19:01:32 +0400

    /build

---
+++ build/precache-manifest.83bc3eb16ac797c0a104546fb0709194.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d9654f33ee69a25877915a58a467dfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c55e79a42032403dc09e",
+    "url": "/static/css/main.9a665b23.chunk.css"
+  },
+  {
+    "revision": "f0c51b2617f35ef9bb09",
+    "url": "/static/js/2.35ba22e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.35ba22e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c55e79a42032403dc09e",
+    "url": "/static/js/main.ffe03bdc.chunk.js"
+  },
+  {
+    "revision": "549d89480a8b9b88c811",
+    "url": "/static/js/runtime-main.f64162a0.js"
+  }
+]);
